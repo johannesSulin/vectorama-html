fa998441e4ffc5ef86084d2cb539d58877df0bb4
Tuukkaleksi <126558697+Tuukkaleksi@users.noreply.github.com>
2023-04-11 12:24:56 +0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,6 +1,1 @@
-///@description Scripts
-window.addEventListener('load', function() {
-    setTimeout(function() {
-        document.querySelector('.overlay').style.display = 'none';
-    }, 1000);
-});
+
